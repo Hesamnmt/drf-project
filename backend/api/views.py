77f21e8d98da761ac395ef74e714e7464d501544
hesamnmt <hesamnmt2001@gmail.com>
2023-05-23 19:42:01 +0330

    permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,22 +2,28 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from blog.models import Article
 from .serializers import ArticleSerializers, UserSerializers
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
+from .permissions import IsSuperUser, IsStaffOrReadOnly, IsAuthorOrReadOnly
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
     
     
-class ArticleDetail(RetrieveUpdateDestroyAPIView):
+class ArticleDetail(RetrieveUpdateDestroyAPIView):        
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
+    permission_classes = [IsAuthorOrReadOnly, IsStaffOrReadOnly]
 
-
+ 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializers
+    permission_classes = [IsSuperUser,]    
     
-    
+     
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializers
+    permission_classes = [IsSuperUser,]
+     
